80fac7bb42922f93478015c35894b636255701d7
Abigail Watson <awatson1978@gmail.com>
2019-12-08 20:08:56 -0600

    serialize-javascript removed from .npm dirs

--- packages/example-plugin/package.js
+++ packages/example-plugin/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'symptomatic:example-plugin',
-    version: '0.2.4',
+    version: '0.2.5',
     summary: 'Example Symptomatic plugin, with dynamic routes and UI elements.',
     git: 'https://github.com/symptomatic/example-plugin',
     documentation: 'README.md'
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
     api.use('session');
     api.use('mongo');  
 
-    api.use('clinical:hl7-resource-patient@5.0.5');
+    api.use('clinical:hl7-resource-patient@5.0.9');
 
     // api.addFiles('assets/NodeOnFhir-ComponentRendering.png', 'client', {isAsset: true});
     // api.addFiles('assets/NodeOnFhir-RecommendedCodingArea.png', 'client', {isAsset: true});
