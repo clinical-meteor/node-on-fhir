3df2ea09bd7853d4750dbed0b83ea1488aafe9fd
Abigail Watson <awatson1978@gmail.com>
2020-04-07 11:46:03 -0500

    cardiac-scorecard v0.2.1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,66 +1,80 @@
 
 
-// Register a service worker
-// https://developers.google.com/web/fundamentals/primers/service-workers
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// // Register a service worker
+// // https://developers.google.com/web/fundamentals/primers/service-workers
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
-// Install a service worker
-// https://developers.google.com/web/fundamentals/primers/service-workers
-self.addEventListener('install', function(event) {
-  // Perform install steps
-});
+// // Install a service worker
+// // https://developers.google.com/web/fundamentals/primers/service-workers
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+// });
 
-var CACHE_NAME = 'fhir-cache';
-var COOKBOOK = 'cookbook';
+// var CACHE_NAME = 'fhir-cache';
+// var COOKBOOK = 'cookbook';
 
-var urlsToCache = [
-  '/',
-  '/client/main.css',
-  '/client/main.js'
-];
+// var urlsToCache = [
+//   '/',
+//   '/client/main.css',
+//   '/client/main.js'
+// ];
 
-let applesauce = ['apples', 'pears', 'sugar', 'cinnemon'];
+// let applesauce = ['apples', 'pears', 'sugar', 'cinnemon'];
 
 
-// self.addEventListener('cookery', function(event) {
-//   console.log("=======================================")
-//   console.log("KAZAAAM!!!!! COOKERY!  PONG, DAMNIT!!!")
-//   console.log("=======================================")
+// // self.addEventListener('cookery', function(event) {
+// //   console.log("=======================================")
+// //   console.log("KAZAAAM!!!!! COOKERY!  PONG, DAMNIT!!!")
+// //   console.log("=======================================")
 
+// //   // Perform install steps
+// //   event.waitUntil(
+// //     caches.open(COOKBOOK)
+// //       .then(function(cache) {
+// //         console.log('Opened cookbook');
+// //         return cache.addAll(applesauce);
+// //       })
+// //   );
+// // });
+
+
+// self.addEventListener('install', function(event) {
 //   // Perform install steps
 //   event.waitUntil(
-//     caches.open(COOKBOOK)
+//     caches.open(CACHE_NAME)
 //       .then(function(cache) {
-//         console.log('Opened cookbook');
-//         return cache.addAll(applesauce);
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
 //       })
 //   );
 // });
 
+// // Cache and return requests
+// // https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Cache and return requests
-// https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests
+// // self.addEventListener('fetch', function(event) {
+// //   event.respondWith(
+// //     caches.match(event.request)
+// //       .then(function(response) {
+// //         // Cache hit - return response
+// //         if (response) {
+// //           return response;
+// //         }
+// //         return fetch(event.request);
+// //       }
+// //     )
+// //   );
+// // });
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
@@ -70,68 +84,54 @@ self.addEventListener('install', function(event) {
 //         if (response) {
 //           return response;
 //         }
-//         return fetch(event.request);
-//       }
-//     )
-//   );
+
+//         return fetch(event.request).then(
+//           function(response) {
+//             // Check if we received a valid response
+//             if(!response || response.status !== 200 || response.type !== 'basic') {
+//               return response;
+//             }
+
+//             // IMPORTANT: Clone the response. A response is a stream
+//             // and because we want the browser to consume the response
+//             // as well as the cache consuming the response, we need
+//             // to clone it so we have two streams.
+//             var responseToCache = response.clone();
+
+//             caches.open(CACHE_NAME)
+//               .then(function(cache) {
+//                 cache.put(event.request, responseToCache);
+//               });
+
+//             return response;
+//           }
+//         );
+//       })
+//     );
 // });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
 
 
 
+// // Update a service worker
+// // https://developers.google.com/web/fundamentals/primers/service-workers#update-a-service-worker  
 
-// Update a service worker
-// https://developers.google.com/web/fundamentals/primers/service-workers#update-a-service-worker  
+// self.addEventListener('activate', function(event) {
 
-self.addEventListener('activate', function(event) {
+//   var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
 
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
 
 
