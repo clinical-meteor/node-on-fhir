69e5e6d167387fea4cd80dadf041ff9f265a8964
Abigail Watson <awatson1978@gmail.com>
2020-04-09 09:08:47 -0500

    syncing to revert

--- app/patient/PatientSidebar.jsx
+++ app/patient/PatientSidebar.jsx
@@ -18,13 +18,57 @@ import ListItemLink from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
 import { Icon } from 'react-icons-kit'
-import {logOut} from 'react-icons-kit/ionicons/logOut'
-import {documentIcon} from 'react-icons-kit/ionicons/documentIcon'
-import {user} from 'react-icons-kit/fa/user'
-import {userMd} from 'react-icons-kit/fa/userMd'
-import {modx} from 'react-icons-kit/fa/modx'
-import {home} from 'react-icons-kit/fa/home'
-import {fire} from 'react-icons-kit/icomoon/fire'
+import { logOut } from 'react-icons-kit/ionicons/logOut'
+import { documentIcon } from 'react-icons-kit/ionicons/documentIcon'
+import { modx } from 'react-icons-kit/fa/modx'
+import { home } from 'react-icons-kit/fa/home'
+import { fire } from 'react-icons-kit/icomoon/fire'
+
+import {userMd} from 'react-icons-kit/fa/userMd'; // Practitioner  
+import {users} from 'react-icons-kit/fa/users'; // Users, Patients
+import {user} from 'react-icons-kit/fa/user';  // Patient 
+
+// import {ic_devices} from 'react-icons-kit/md/ic_devices';  // Devices
+// import {ic_transfer_within_a_station} from 'react-icons-kit/md/ic_transfer_within_a_station' // Encounters 
+// import {ic_local_pharmacy} from 'react-icons-kit/md/ic_local_pharmacy'  // Medication, MedicationStatement, MedicationOrder  
+// import {heartbeat} from 'react-icons-kit/fa/heartbeat' // Condition
+// import {erlenmeyerFlask} from 'react-icons-kit/ionicons/erlenmeyerFlask' // Substance  
+// import {hospitalO} from 'react-icons-kit/fa/hospitalO' // Hospital  
+// import {bath} from 'react-icons-kit/fa/bath'  // Procedure  
+// import {suitcase} from 'react-icons-kit/fa/suitcase' // Bundle
+// import {notepad} from 'react-icons-kit/ikons/notepad'  // CarePlan ?
+// import {iosPulseStrong} from 'react-icons-kit/ionicons/iosPulseStrong' // Pulse, Condition  
+// import {location} from 'react-icons-kit/typicons/location' // Location
+// import {eyedropper} from 'react-icons-kit/fa/eyedropper'
+
+// import {ic_hearing} from 'react-icons-kit/md/ic_hearing'  // Condition?
+// import {ic_fingerprint} from 'react-icons-kit/md/ic_fingerprint' // Biometric
+// import {ic_accessible} from 'react-icons-kit/md/ic_accessible' // Devices
+// import {thermometer3} from 'react-icons-kit/fa/thermometer3' // Observation  
+// import {stethoscope} from 'react-icons-kit/fa/stethoscope' // Device
+// import {umbrella} from 'react-icons-kit/fa/umbrella' // ExplanationOfBeneft,
+// import {dashboard} from 'react-icons-kit/fa/dashboard' //Dashboard
+// import {flask} from 'react-icons-kit/fa/flask' // Substance 
+// import {cameraRetro} from 'react-icons-kit/fa/cameraRetro' // ImagingStudy
+// import {film} from 'react-icons-kit/fa/film' // Media 
+// import {image} from 'react-icons-kit/fa/image' // Media 
+// import {envelopeO} from 'react-icons-kit/fa/envelopeO' // Correspondence 
+// import {eye} from 'react-icons-kit/fa/eye' // BodySite
+// import {barcode} from 'react-icons-kit/fa/barcode' // Barcode  
+// import {ambulance} from 'react-icons-kit/fa/ambulance' // Ambulance   
+// import {medkit} from 'react-icons-kit/fa/medkit'  // SmartKit  
+// import {desktop} from 'react-icons-kit/fa/desktop' //Desktop  
+// import {tablet} from 'react-icons-kit/fa/tablet' // Tablet 
+// import {mobile} from 'react-icons-kit/fa/mobile' // Mobile 
+// import {laptop} from 'react-icons-kit/fa/laptop' // Laptop  
+// import {wheelchair} from 'react-icons-kit/fa/wheelchair' // Wheelchair   
+// import {signing} from 'react-icons-kit/fa/signing' // Handwash / Signing  
+// import {addressCardO} from 'react-icons-kit/fa/addressCardO'  // Address Card  
+// import {addressBook} from 'react-icons-kit/fa/addressBook' // Address Book  
+// import {iosNutrition} from 'react-icons-kit/ionicons/iosNutrition' // Nutrition  
+// import {nuclear} from 'react-icons-kit/ionicons/nuclear' // Radiology  
+// import {pipette} from 'react-icons-kit/typicons/pipette' // Immunization ?
+
 
 
 
@@ -101,6 +145,9 @@ const styles = theme => ({
     justifyContent: 'flex-end',
     padding: '0 8px',
     ...theme.mixins.toolbar
+  },
+  divider: {
+    height: '2px'
   }
 });
 
@@ -141,7 +188,7 @@ export function PatientSidebar(props){
         </ListItem>
       );
 
-      constructionZone.push(<Divider key='construction-hr' />);
+      constructionZone.push(<Divider className={props.classes.divider} key='construction-hr' />);
     }
   }
 
@@ -161,7 +208,7 @@ export function PatientSidebar(props){
         </ListItem>
       );
 
-      fhirResources.push(<Divider key='hra' />);
+      fhirResources.push(<Divider className={props.classes.divider} key='hra' />);
     //}
   }
 
@@ -180,9 +227,26 @@ export function PatientSidebar(props){
         });    
       }
     }); 
-  
+
+    dynamicModules.push(<Divider className={props.classes.divider} key='dynamic-modules-hr' />);  
     logger.data('PatientSidebar.dynamicModules', dynamicModules);
   }
+
+  let sidebarWorkflows = [];
+  if(get(Meteor, 'settings.public.defaults.sidebar.menuItems.SidebarWorkflows')){
+    Object.keys(Package).forEach(function(packageName){
+      if(Package[packageName].SidebarWorkflows){
+        // we try to build up a route from what's specified in the package
+        Package[packageName].SidebarWorkflows.forEach(function(element){
+          sidebarWorkflows.push(element);      
+        });    
+      }
+    }); 
+
+    sidebarWorkflows.push(<Divider className={props.classes.divider} key='workflows-hr' />);  
+    logger.data('PatientSidebar.sidebarWorkflows', sidebarWorkflows);
+  }
+  
 
 
   //----------------------------------------------------------------------
@@ -191,6 +255,105 @@ export function PatientSidebar(props){
   var dynamicElements = [];
   if(get(Meteor, 'settings.public.defaults.sidebar.menuItems.DynamicModules')){
     dynamicModules.map(function(element, index){ 
+
+      let clonedIcon;
+
+      switch (element.iconName) {
+        case "FaUserInjured":
+          element.icon = <Icon icon={user} />
+          break;
+        case "FaUserMd":
+          element.icon = <Icon icon={userMd} />
+          break;
+        case "GoKebabHorizontal":
+          element.icon = <Icon icon={fire} />
+          break;
+        case "GoFlame":
+          element.icon = <Icon icon={fire} />
+          break;
+        case "MdLineWeight":
+          element.icon = <Icon icon={fire} />
+          break;
+
+        // case "suitcase":
+        //   element.icon = <Icon icon={suitcase} />
+        //   break;
+        // case "notepad":
+        //   element.icon = <Icon icon={notepad} />
+        //   break;
+        // case "heartbeat":
+        //   element.icon = <Icon icon={heartbeat} />
+        //   break;
+        // case "ic_devices":
+        //   element.icon = <Icon icon={ic_devices} />
+        //   break;
+        // case "ic_transfer_within_a_station":
+        //   element.icon = <Icon icon={ic_transfer_within_a_station} />
+        //   break;
+        // case "eyedropper":
+        //   element.icon = <Icon icon={eyedropper} />
+        //   break;
+        // case "location":
+        //   element.icon = <Icon icon={location} />
+        //   break;
+        // case "erlenmeyerFlask":
+        //   element.icon = <Icon icon={erlenmeyerFlask} />
+        //   break;
+        // case "ic_local_pharmacy":
+        //   element.icon = <Icon icon={ic_local_pharmacy} />
+        //   break;
+        // case "iosPulseStrong":
+        //   element.icon = <Icon icon={iosPulseStrong} />
+        //   break;
+        // case "hospitalO":
+        //   element.icon = <Icon icon={hospitalO} />
+        //   break;
+        // case "users":
+        //   element.icon = <Icon icon={users} />
+        //   break;
+        // case "userMd":
+        //   element.icon = <Icon icon={userMd} />
+        //   break;
+        // case "bath":
+        //   element.icon = <Icon icon={bath} />
+        //   break;
+          
+
+        default:
+          break;
+      }
+
+      // we want to pass in the props
+      if(element.icon){
+        clonedIcon = React.cloneElement(element.icon, {
+          className: props.classes.drawerIcons 
+        });
+      } else {
+        clonedIcon = <Icon icon={fire} className={props.classes.drawerIcons} />
+      }
+
+      // the excludes array will hide routes
+      if(!get(Meteor, 'settings.public.defaults.sidebar.hidden', []).includes(element.to)){
+        dynamicElements.push(
+          <ListItem key={index} button onClick={function(){ openPage(element.to); }} >
+            <ListItemIcon >
+              { clonedIcon }
+            </ListItemIcon>
+            <ListItemText primary={element.primaryText} className={props.classes.drawerText}  />
+          </ListItem>
+        );
+      }
+    });
+    dynamicElements.push(<Divider className={props.classes.divider} key="dynamic-modules-hr" />);
+  }
+
+
+  //----------------------------------------------------------------------
+  // Workflow Modules  
+
+  var workflowElements = [];
+  if(get(Meteor, 'settings.public.defaults.sidebar.menuItems.SidebarWorkflows')){
+    sidebarWorkflows.map(function(element, index){ 
 
       let clonedIcon;
 
@@ -225,8 +388,8 @@ export function PatientSidebar(props){
       }
 
       // the excludes array will hide routes
-      if(!get(Meteor, 'settings.public.defaults.sidebar.hidden', []).includes(element.to)){
-        dynamicElements.push(
+      if(!get(Meteor, 'settings.public.defaults.sidebar.hiddenWorkflow', []).includes(element.to)){
+        workflowElements.push(
           <ListItem key={index} button onClick={function(){ openPage(element.to); }} >
             <ListItemIcon >
               { clonedIcon }
@@ -236,7 +399,7 @@ export function PatientSidebar(props){
         );
       }
     });
-    dynamicElements.push(<Divider key="dynamic-modules-hr" />);
+    workflowElements.push(<Divider className={props.classes.divider} key="workflow-modules-hr" />);
   }
 
 
@@ -251,7 +414,7 @@ export function PatientSidebar(props){
         </ListItemIcon>
         <ListItemText primary="Home Page" className={props.classes.drawerText}  />
       </ListItem>);    
-      homePage.push(<Divider key="home-page-hr" />);
+      homePage.push(<Divider className={props.classes.divider} key="home-page-hr" />);
   };
 
 
@@ -329,11 +492,18 @@ export function PatientSidebar(props){
     <div id='patientSidebar'>
       { homePage }
 
+      <div id='patientWorkflowElements' key='patientWorkflowElements'>
+        {/* <h4>Workflow</h4> */}
+        { workflowElements }   
+      </div>
+
       <div id='patientDynamicElements' key='patientDynamicElements'>
+        {/* <h4>Data</h4> */}
         { dynamicElements }   
       </div>
 
       { fhirResources }         
+
       { constructionZone }         
 
       { themingElements }
