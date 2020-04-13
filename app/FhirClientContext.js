73c023f7de6cd630b8291abdf52b6d7d153016d1
Abigail Watson <awatson1978@gmail.com>
2020-04-13 00:49:16 -0500

    delinting; smart on fhir cerner debugging

--- app/FhirClientContext.js
+++ app/FhirClientContext.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default const FhirClientContext = React.createContext({
+export const FhirClientContext = React.createContext({
     client: null,
     setClient: function(client) {
         context.client = client;
     }
 });
+
+export default FhirClientContext;
