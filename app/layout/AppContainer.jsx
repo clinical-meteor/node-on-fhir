b077e3288978001f59de03a1feeb897eac01b90f
Abigail Watson <abigailwatson@JurrassoControl.attlocal.net>
2021-01-25 22:02:37 -0600

    code compiles; no logging on core

--- app/layout/AppContainer.jsx
+++ app/layout/AppContainer.jsx
@@ -2,9 +2,6 @@
 import React from 'react';
 import { BrowserRouter, withRouter, Router } from "react-router-dom";
 
-import { get } from 'lodash';
-
-import { Meteor } from 'meteor/meteor';
 
 import { ThemeProvider } from '@material-ui/styles';
 // import { blue400, blue600, green600, green800 } from 'material-ui/styles/colors';
@@ -20,7 +17,112 @@ import 'setimmediate';
 
 import { PatientTable } from 'fhir-starter';
 
-// import logger from '../Logger';
+import { createLogger, addColors, format, transports } from 'winston';
+import { get } from 'lodash';
+import { Meteor } from 'meteor/meteor';
+
+
+
+  // some functions that do log level filtering
+  const LEVEL = Symbol.for('level');
+  function filterOnly(level) {
+    return format(function (info) {
+      if (info[LEVEL] === level) {
+        return info;
+      }
+    })();
+  }
+
+  function hideDataLogLevel() {
+    return format(function (info) {
+      if (info[LEVEL] !== 'data') {
+        return info;
+      }
+    })();
+  }
+
+  function onlyDisplayDataLogLevel() {
+    return format(function (info) {
+      if (info[LEVEL] === 'data') {
+        return info;
+      }
+    })();
+  }
+
+
+// lets create a global logger
+export const logger = createLogger({
+  level: get(Meteor, 'settings.public.loggingThreshold') ,
+  levels: {
+    error: 0, 
+    warn: 1, 
+    info: 2, 
+    verbose: 3, 
+    debug: 4, 
+    trace: 5, 
+    data: 6 
+  },
+  // defaultMeta: {tags: ['client_app']},
+  // defaultMeta: { app: get(Meteor, 'settings.public.title') },
+  transports: [
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
+
+    new transports.Console({
+      colorize: true,
+      format: format.combine(
+        hideDataLogLevel(),
+        format.colorize(),
+        format.simple(),
+        format.splat(),
+        format.timestamp()
+      )
+    }),
+
+    new transports.Console({
+      colorize: true,
+      format: format.combine(
+        onlyDisplayDataLogLevel(),
+        format.simple(),
+        format.splat(),
+        format.prettyPrint()
+      )
+    })
+  ],
+  exitOnError: false
+});
+
+addColors({
+  error: "red", 
+  warn: "yellow", 
+  info: "white bold", 
+  verbose: "green", 
+  debug: "cyan", 
+  trace: 'cyan',
+  data: "grey" 
+});
+  
+// what is the logging threshold set in the Meteor.settings file?
+logger.verbose('Setting the logging threshold to: ' + get(Meteor, 'settings.public.loggingThreshold'))
+
+
+// introspection for the win
+// logger.info('Starting the Winston Logging Service');
+// logger.data('Winston Logging Service', {data: logger}, {source: "AppContainer.jsx"});
+
+// attaching to the global scope is not recommending
+// logging is one debatable exception to the general rule, however
+if(typeof window === "object"){
+  window.logger = global.logger = logger;
+}
+
+// ironically telling the logger where to write the error message when it fails
+logger.on('error', function (error) { 
+  console.error('Winston just blew up.', error)
+});
 
 
 // Global App-Wide Session Variables
@@ -43,6 +145,7 @@ Meteor.startup(function(){
     Session.set('appHeight', window.innerHeight);
     Session.set('appWidth', window.innerWidth);  
   }
+  
   
   // var LocalDb = minimongo.MemoryDb;
  
