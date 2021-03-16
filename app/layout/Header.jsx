23a19a17c24c75417c228f8bebab01267545a357
Abigail Watson <abigailwatson@JurrassControl2.attlocal.net>
2021-03-16 15:28:50 -0500

    conflicts should be resolved

--- app/layout/Header.jsx
+++ app/layout/Header.jsx
@@ -241,8 +241,8 @@ function Header(props) {
             );
           }
         }
-      }
-    })       
+      })
+    }     
   }
 
 
@@ -338,11 +338,14 @@ function Header(props) {
     }
   }
 
+<<<<<<< HEAD
+=======
   let titleClass = componentStyles.title;
   if(Meteor.isCordova){
     titleClass = componentStyles.title_cordova;
   }
 
+>>>>>>> development
   return (
     <div id="header" className="headerNavContainer" position="fixed" className={componentStyles.headerNavContainer}>
       <div style={{paddingTop: '10px'}}>
